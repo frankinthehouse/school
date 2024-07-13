8747b2f20195c0f50a09cb51df29d5765362d5b5
frank <kiol726a@gmail.com>
2024-07-13 11:27:52 +0800

    tgbotupdate

---
+++ tgbot.sh
@@ -0,0 +1,4 @@
+TGBOT=6003194653:AAF5PviH8BBqMRgaxhKfH-coLc-OwTPNIME
+CHAT_ID=222536676
+
+curl -s -o /dev/null -X POST https://api.telegram.org/bot$TGBOT/sendMessage -d chat_id=$CHAT_ID -d text="$(date +'%Y%b%d %H:%M:%S') : github update"
