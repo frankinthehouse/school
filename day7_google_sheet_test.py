2a2a5d49e1cfd9d536f4ff08c8dd503b852bdfaf
frank <kiol726a@gmail.com>
2024-07-20 14:01:22 +0800

    v7

--- day7_google_sheet_test.py
+++ day7_google_sheet_test.py
@@ -1,8 +1,8 @@
 # google 表格 的 模組
 import pygsheets
 
-# 吃 google 的設定黨 
-gs = pygsheets.authorize(service_file = ".infinite-glow-430001-e9-82f4dd2eb476.json")
+# 吃 google 的設定黨 git 
+gs = pygsheets.authorize(service_file = "infinite-glow-430001-e9-82f4dd2eb476.json")
 
 # 使用 url 開啟 表格 
 sht = gs.open_by_url("https://docs.google.com/spreadsheets/d/1ZZA1Wx6XFdIMKmMf0rl4awiVcCy1FxcnDo9drtpwYkY/edit?gid=0#gid=0")
